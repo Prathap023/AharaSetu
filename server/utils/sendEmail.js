c074acbb5241a2cbd77b6613c7a2d2aa457649f6
Prathap023 <prathap18at18@gmail.com>
2026-05-02 17:01:39 +0530

    Fix email TLS for Render

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,10 +3,13 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
