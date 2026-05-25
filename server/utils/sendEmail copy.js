e3251c7008cca9fcf3fdbf762a209cee7d06aaa8
Prathap023 <prathap18at18@gmail.com>
2026-05-25 21:58:23 +0530

    Fix: emails fixed image in email also

--- server/utils/sendEmail copy.js
+++ server/utils/sendEmail copy.js
@@ -24,7 +24,7 @@ const createEmailTemplate = ({
   ">
 
     <img 
-      src="/AharaSetu-logo-round.png"
+      src="client/public/AharaSetu-logo-round.png"
       width="120"
       alt="AharaSetu"
       style="
